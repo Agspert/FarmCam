83eef1ad0eadacb1eb02d0291da431ff2261a5e6
Prince86 <pkbghl90@gmail.com>
2023-09-28 22:37:45 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,32 @@ import THREECanvas from "@/components/viewer";
 import Model from "@//components/model";
 import { RWebShare } from "react-web-share";
 
-import { SendHorizonal, MapPin, Volume2, VolumeX, Play, Pause } from "lucide-react";
+import {
+  SendHorizonal,
+  MapPin,
+  Play,
+  Pause,
+} from "lucide-react";
 import { Button, buttonVariants } from "./components/ui/button";
 import { cn } from "./lib/utils";
 import { useEffect, useRef, useState } from "react";
-import FBXModel from "./components/fbxModel";
 function App() {
   const lat = 27.629683;
   const lon = 78.106681;
 
   const [domTouched, setDomTouched] = useState(false);
-  const [audio] = useState(() => new Audio("./Love-Me-Like-You-Do.mp3"));
+  const [audio] = useState(() => new Audio("./main.mp3"));
   const [progress, setProgress] = useState("0");
   const [seconds, setSeconds] = useState(1);
   const [width, setWidth] = useState("0px");
 
-  const [paused, setPaused] = useState<boolean>(false)
+  const [paused, setPaused] = useState<boolean>(false);
   const [caption, setCaption] = useState<string | undefined>(undefined);
 
   useEffect(() => {
     let interval: any;
     if (domTouched) {
-      console.log("in if")
+      console.log("in if");
       audio.play();
       interval = setInterval(() => {
         setSeconds((prev) => prev + 1);
@@ -38,251 +42,53 @@ function App() {
   }, [domTouched]);
 
   const dd = [
-    { id: 0, startTime: 0, endTime: 5, text: "Love Me Like You Do" },
-    { id: 1, startTime: 5, endTime: 10, text: "Ellie Goulding" },
+    {
+      id: 1,
+      startTime: 0,
+      endTime: 10.2,
+      text: "Thank you for purchasing this tea grown right here in my farm that you see behind! I am Prema, a farmer from the scenic hills of tawang, a small town in Arunachal Pradesh.",
+    },
     {
       id: 2,
-      startTime: 19.39,
-      endTime: 22.54,
-      text: "You're the light, you're the night",
+      startTime: 10.2,
+      endTime: 17.11,
+      text: "Generations of my family have tilled this land, facing the uphill battle of marketing our produce as small farmers.",
     },
     {
       id: 3,
-      startTime: 22.54,
-      endTime: 24.42,
-      text: "You're the colour of my blood",
+      startTime: 17.11,
+      endTime: 21.17,
+      text: "Our farm boasts diverse crops like tea and exotic fruits.",
     },
     {
       id: 4,
-      startTime: 24.42,
-      endTime: 27.17,
-      text: "You're the cure, you're the pain",
+      startTime: 21.17,
+      endTime: 26.55,
+      text: "Connecting with consumers was a challenge until we embraced agspeak's digital technologies.",
     },
     {
       id: 5,
-      startTime: 27.17,
-      endTime: 31.92,
-      text: "You're the only thing I wanna touch",
+      startTime: 26.55,
+      endTime: 31.64,
+      text: "Now, we are gaining more visibility, thus reaching end consumers efficiently.",
     },
     {
       id: 6,
-      startTime: 31.92,
-      endTime: 36.31,
-      text: "Never knew that it could mean so much,",
+      startTime: 31.64,
+      endTime: 39.34,
+      text: "By the way, you can tap on the share button to share my story with your friends, or use the location button to find my farm on the map.",
     },
-    { id: 7, startTime: 36.31, endTime: 39.2, text: "so much" },
+    {
+      id: 7,
+      startTime: 39.34,
+      endTime: 44.36,
+      text: "Do visit my farm to enjoy a day full of nature and fresh food from our ethnic kitchen.",
+    },
     {
       id: 8,
-      startTime: 39.84,
-      endTime: 42.47,
-      text: "You're the fear, I don't care",
-    },
-    {
-      id: 9,
-      startTime: 42.47,
-      endTime: 44.87,
-      text: "'Cause I've never been so high",
-    },
-    { id: 10, startTime: 44.87, endTime: 47.12, text: "Follow me to the dark" },
-    {
-      id: 11,
-      startTime: 47.12,
-      endTime: 52.51,
-      text: "Let me take you past our satellites",
-    },
-    {
-      id: 12,
-      startTime: 52.51,
-      endTime: 56.76,
-      text: "You can see the world you brought to life,",
-    },
-    { id: 13, startTime: 56.76, endTime: 58.65, text: "to life" },
-    {
-      id: 14,
-      startTime: 60.05,
-      endTime: 64.68,
-      text: "So love me like you do, lo-lo-love me like you do",
-    },
-    {
-      id: 15,
-      startTime: 64.68,
-      endTime: 70.18,
-      text: "Love me like you do, lo-lo-love me like you do",
-    },
-    {
-      id: 16,
-      startTime: 70.18,
-      endTime: 76.43,
-      text: "Touch me like you do, to-to-touch me like you do",
-    },
-    {
-      id: 17,
-      startTime: 76.43,
-      endTime: 79.83,
-      text: "What are you waiting for?",
-    },
-    { id: 18, startTime: 79.83, endTime: 82.83, text: "Fading in, fading out" },
-    {
-      id: 19,
-      startTime: 82.83,
-      endTime: 85.33,
-      text: "On the edge of paradise",
-    },
-    {
-      id: 20,
-      startTime: 85.33,
-      endTime: 92.59,
-      text: "Every inch of your skin is a holy grail I've got to find",
-    },
-    {
-      id: 21,
-      startTime: 92.59,
-      endTime: 99.08,
-      text: "Only you can set my heart on fire, on fire",
-    },
-    {
-      id: 22,
-      startTime: 99.479,
-      endTime: 105.13,
-      text: "Yeah, I'll let you set the pace",
-    },
-    {
-      id: 23,
-      startTime: 105.13,
-      endTime: 110.52000000000001,
-      text: "'Cause I'm not thinking straight",
-    },
-    {
-      id: 24,
-      startTime: 110.52000000000001,
-      endTime: 117.52000000000001,
-      text: "My head's spinning around I can't see clear no more",
-    },
-    {
-      id: 25,
-      startTime: 117.52000000000001,
-      endTime: 120.77,
-      text: "What are you waiting for?",
-    },
-    {
-      id: 26,
-      startTime: 120.77,
-      endTime: 126.42,
-      text: "Love me like you do, lo-lo-love me like you do (like you do)",
-    },
-    {
-      id: 27,
-      startTime: 126.42,
-      endTime: 130.32,
-      text: "Love me like you do, lo-lo-love me like you do",
-    },
-    {
-      id: 28,
-      startTime: 130.32,
-      endTime: 137.24,
-      text: "Touch me like you do, to-to-touch me like you do",
-    },
-    {
-      id: 29,
-      startTime: 137.24,
-      endTime: 141.13,
-      text: "What are you waiting for?",
-    },
-    {
-      id: 30,
-      startTime: 141.13,
-      endTime: 146.639,
-      text: "Love me like you do, lo-lo-love me like you do (like you do)",
-    },
-    {
-      id: 31,
-      startTime: 146.639,
-      endTime: 151.13,
-      text: "Love me like you do, lo-lo-love me like you do (yeah)",
-    },
-    {
-      id: 32,
-      startTime: 151.13,
-      endTime: 157.38,
-      text: "Touch me like you do, to-to-touch me like you do",
-    },
-    {
-      id: 33,
-      startTime: 157.38,
-      endTime: 161.38,
-      text: "What are you waiting for?",
-    },
-    {
-      id: 34,
-      startTime: 171.139,
-      endTime: 176.55,
-      text: "I'll let you set the pace",
-    },
-    {
-      id: 35,
-      startTime: 176.55,
-      endTime: 180.7,
-      text: "'Cause I'm not thinking straight",
-    },
-    {
-      id: 36,
-      startTime: 180.7,
-      endTime: 190.21,
-      text: "My head's spinning around I can't see clear no more",
-    },
-    {
-      id: 37,
-      startTime: 190.21,
-      endTime: 193.84,
-      text: "What are you waiting for?",
-    },
-    {
-      id: 38,
-      startTime: 193.84,
-      endTime: 199.59,
-      text: "Love me like you do, lo-lo-love me like you do (like you do)",
-    },
-    {
-      id: 39,
-      startTime: 199.59,
-      endTime: 204.09,
-      text: "Love me like you do, lo-lo-love me like you do (yeah)",
-    },
-    {
-      id: 40,
-      startTime: 204.09,
-      endTime: 211.23,
-      text: "Touch me like you do, to-to-touch me like you do",
-    },
-    {
-      id: 41,
-      startTime: 211.23,
-      endTime: 214.73,
-      text: "What are you waiting for?",
-    },
-    {
-      id: 42,
-      startTime: 214.73,
-      endTime: 219.62,
-      text: "Love me like you do, lo-lo-love me like you do (like you do)",
-    },
-    {
-      id: 43,
-      startTime: 219.62,
-      endTime: 224.77,
-      text: "Love me like you do, lo-lo-love me like you do (oh)",
-    },
-    {
-      id: 44,
-      startTime: 224.77,
-      endTime: 231.77,
-      text: "Touch me like you do, to-to-touch me like you do",
-    },
-    {
-      id: 45,
-      startTime: 231.77,
-      endTime: 234.77,
-      text: "What are you waiting for?",
+      startTime: 44.36,
+      endTime: 48.73,
+      text: "Meanwhile, enjoy your tea grown right here at my farm!",
     },
   ];
 
@@ -302,22 +108,22 @@ function App() {
 
   useEffect(() => {
     // if (seconds <= audio.duration) {
-      const chuncks = Math.ceil(window.innerWidth / audio.duration);
-      let width = chuncks * Math.ceil(audio.currentTime);
-      width = width > window.innerWidth ? window.innerWidth : width;
-      setWidth(`${width}px`);
+    const chuncks = Math.ceil(window.innerWidth / audio.duration);
+    let width = chuncks * Math.ceil(audio.currentTime);
+    width = width > window.innerWidth ? window.innerWidth : width;
+    setWidth(`${width}px`);
     // }
   }, [audio.currentTime]);
 
   const handleClick = () => {
-    if(paused){
+    if (paused) {
       audio.play();
-      setPaused(false)
+      setPaused(false);
     } else {
-      audio.pause()
-      setPaused(true)
+      audio.pause();
+      setPaused(true);
     }
-  }
+  };
 
   return (
     <div
@@ -332,9 +138,9 @@ function App() {
       <div className="absolute top-[50%] translate-y-[-50%] right-4 flex flex-col gap-2 z-50">
         <RWebShare
           data={{
-            text: "this is dummy text",
+            text: "Prema's Farm",
             url: `${window.location.href}`,
-            title: "This is just a dummy title for testing purposes",
+            title: "",
           }}
         >
           <Button size="icon" className=" rounded-full bg-slate-100">
@@ -352,20 +158,24 @@ function App() {
         >
           <MapPin className="text-black" />
         </a>
-        <Button size="icon" className="rounded-full bg-slate-100" onClick={handleClick} >
-          {paused ? 
-          <Play className="w-4 h-4 text-black" />
-          :
-          <Pause className="w-4 h-4 text-black" /> 
-        }
-          </Button>
+        <Button
+          size="icon"
+          className="rounded-full bg-slate-100"
+          onClick={handleClick}
+        >
+          {paused ? (
+            <Play className="w-4 h-4 text-black" />
+          ) : (
+            <Pause className="w-4 h-4 text-black" />
+          )}
+        </Button>
       </div>
       <div
         style={{ width: width }}
-        className="absolute top-0 border-2 border-red-600 z-50"
+        className="absolute top-0 border-2 border-yellow-400 rounded-sm z-50"
       ></div>
-      <p className="absolute left-[50%] translate-x-[-50%] bottom-12 text-black font-semibold text-2xl rounded-sm z-40 bg-yellow-600 px-2 backdrop-blur-sm">
-        {caption}
+      <p className="absolute left-[50%] translate-x-[-50%] w-[90vw] bottom-12 mb-4 text-black font-semibold text-2xl rounded-sm z-40">
+        <span className="bg-yellow-600">{caption}</span>
       </p>
     </div>
   );
